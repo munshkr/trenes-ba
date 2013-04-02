7e5fcd6383cc37da91a416f038570129354b16d5
Damián Silvani <munshkr@gmail.com>
2013-04-01 21:06:03 -0300

    Finish Schedule scraper

--- lib/schedule_scraper.rb
+++ lib/schedule_scraper.rb
@@ -3,7 +3,7 @@ require "nokogiri"
 
 class ScheduleScraper < Scraper
   URL = "http://www.mitresarmiento.com.ar/horarios.asp"
-  RETRIES = 3
+  RETRIES = 10
 
   DAYS_IDS = {
     monday_to_friday: 2,
@@ -24,26 +24,43 @@ class ScheduleScraper < Scraper
   def run
     DAYS_IDS.each_key do |days|
       ROUTE_PER_BRANCH.each do |branch, route|
-        p = self.class.path(branch, Date.today, days)
-        FileUtils.mkdir_p(File.dirname(p))
+        [:departure, :return].each do |way|
+          p = self.class.path(branch, Date.today, days, way)
+          FileUtils.mkdir_p(File.dirname(p))
 
-        File.open(p, 'w') do |f|
-          table = nil
+          File.open(p, 'w') do |f|
+            route = route.reverse if way == :return
 
-          # TODO build table
-          route.each_slice(2) do |a, b|
-            slice_tbl = download_table(a, b, days)
-            # ...
+            only_save_b = false
+            route.each_slice(2) do |a, b|
+              only_save_b = true if b.nil?
+
+              if only_save_b
+                # This is the odd-numbered terminal station, so make a request
+                # for the previous station and this one.
+                b = a
+                a = route[-2]
+              end
+
+              a_to_b = case way
+                when :departure then download_table(a, b, days)
+                when :return    then download_table(b, a, days)
+                else raise "Unknown way"
+              end
+
+              ta, tb = a_to_b.transpose
+
+              f.puts ta.join(",") if not only_save_b
+              f.puts tb.join(",")
+            end
           end
-
-          f.puts table
         end
       end
     end
   end
 
-  def self.path(branch, date, days)
-    File.join(DATA_PATH, "schedule", branch.to_s, "#{date.strftime("%Y-%m")}-#{days}.csv")
+  def self.path(branch, date, days, way)
+    File.join(DATA_PATH, "schedule", branch.to_s, "#{date.strftime("%Y-%m")}-#{way}-#{days}.csv")
   end
 
 
@@ -53,9 +70,9 @@ private
     params = {
       "EstOri" => origin,
       "EstDes" => destination,
-      "Hsale" => "00:00",
+      "Hsale"  => "00:00",
       "Hllega" => "24:00",
-      "Dias" => DAYS_IDS[days],
+      "Dias"   => DAYS_IDS[days],
     }
 
     html = nil
